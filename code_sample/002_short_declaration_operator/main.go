cfa271c776f0769dd3b805fb014779b264f9ca04
rootkit-io <babexexploitking@gmail.com>
2021-05-18 20:35:09 +0530

    keep going

---
+++ code_sample/002_short_declaration_operator/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 455 // we have to assign var at first by using : with =
+	fmt.Println(x)
+	x = 999 // we can reassign value by just using =
+	fmt.Println(x)
+	y := 660 + 6
+	fmt.Println(y)
+	z := "JamesBond007"
+	fmt.Println(z)
+
+}

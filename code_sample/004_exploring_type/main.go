5aa3bd2d5ab2231a7fdfa2f14d624a4da438dec9
rootkit-io <babexexploitking@gmail.com>
2021-05-20 17:24:29 +0530

    keep_going

---
+++ code_sample/004_exploring_type/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var y = 42
+
+// DECLARE the VARIABLE with the IDENTIFIER "z"
+// is of TYPE string
+// and I ASSIGN the value "Shaken, not stirred"
+
+var z string = "Shaken, not stirred"
+
+var a string = `Jesse said,
+"Shaken,
+
+not stirred"`
+
+// this is a STATIC programming language
+// a VARIABLE is DECLARED to hold the VALUE of a certain TYPE
+// not a DYNAMIC programming language
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	//z = 43
+	//fmt.Println(z)
+	//fmt.Printf("%T\n", z)
+}

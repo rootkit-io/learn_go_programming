4caac556d2cc1f7c6b76a017ee0311f5c8cd3da8
rootkit-io <babexexploitking@gmail.com>
2021-05-20 15:41:58 +0530

    keep_going

---
+++ code_sample/003_the_var_keyword/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// DECLARE the variable "y"
+// ASSIGN the value 43
+// declare & assign = initilization
+
+var y = 433
+
+// DECLARE with the VARIABLE with the IDENTIFIER "z"
+// and that the VARIABLE with the IDENTIFIER "z" is of TYPE int
+// ASSIGNS the ZERO VALUE of TYPE int to "z"
+// FALSE for booleans, 0 for integers, 0.0 for floats, "" for strings,
+// nil for pointers, functions, interfaces, slices, channels and maps.
+var z int
+
+func main() {
+	// short declaration operator
+	// DECLARE a variable and ASSIGN a VALUE (of a certain TYPE)
+	x := 442
+	fmt.Println(x)
+
+	fmt.Println(y)
+
+	foo()
+
+	fmt.Println(z)
+}
+
+func foo() {
+	fmt.Println("again:", y)
+}

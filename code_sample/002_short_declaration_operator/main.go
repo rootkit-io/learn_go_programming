12c30d8e84212485e9d852edb4c946733a14c2fa
rootkit-io <babexexploitking@gmail.com>
2021-05-22 00:37:18 +0530

    keep_going

--- code_sample/002_short_declaration_operator/main.go
+++ code_sample/002_short_declaration_operator/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	x := 455 // we have to declare operator at first by using : with =
 	fmt.Println(x)
-	x = 999 // we can reassign value by just using = 
+	x = 999 // we can reassign value by just using =
 	fmt.Println(x)
 	y := 660 + 6
 	fmt.Println(y)
---
+++ code_sample/005_zero_value/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+var z string
+var y int
+
+func main() {
+	// DECLARE the variable to be of certain TYPE
+	// and then ASSIGN a VALUE of that variable
+	fmt.Println("the value of z", z, "ending")
+	fmt.Printf("%T\n", z)
+	z = "007 james bond"
+	fmt.Println(z)
+	fmt.Printf("%T\n", z)
+
+	fmt.Println("the value of y", y)
+	fmt.Printf("%T\n", y)
+}
+
+// false for booleans
+// 0 	 for int
+// 0.0   for float
+// "" 	 for strings
+// nil   for
+//			* pointers
+//			* functions
+//			* interfaces
+//			* slices
+//			* channels
+//			* maps
+
+// TIPS: use short declaration as much as possible
+// : use var for zero value and package level scope

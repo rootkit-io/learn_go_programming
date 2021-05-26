cbfdbe550a1ef30e497254902546a21c4e5f434b
rootkit-io <babexexploitking@gmail.com>
2021-05-26 19:50:12 +0530

    keep_going

---
+++ programming_fundamentals/bool/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var x bool
+
+func main() {
+	fmt.Println(x)
+	x = true
+	fmt.Println(x)
+}
---
+++ programming_fundamentals/bool/main_2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 34
+	b := 55
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a > b)
+	fmt.Println(a < b)
+	fmt.Println(a <= b)
+	fmt.Println(a >= b)
+
+}

00a4ac3e094b2f870341fffdd4e2324b95724258
rootkit-io <babexexploitking@gmail.com>
2021-05-31 15:34:17 +0530

    Keep_going

--- programming_fundamentals/iota/main.go
+++ programming_fundamentals/iota/main.go
@@ -8,11 +8,25 @@ const (
 	c = iota
 )
 
+const (
+	d = iota
+	e
+	f
+)
+
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
 	fmt.Printf("%T\n", a)
 	fmt.Printf("%T\n", b)
 	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", d)
+	fmt.Printf("%T\n", e)
+	fmt.Printf("%T\n", f)
 }
+
+// Keep Going

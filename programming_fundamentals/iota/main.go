3268dafbbc14fb5f66b8ee1f01112f1a56482ae0
rootkit-io <babexexploitking@gmail.com>
2021-05-31 14:39:38 +0530

    keep_going

---
+++ programming_fundamentals/iota/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	a = iota
+	b = iota
+	c = iota
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+}

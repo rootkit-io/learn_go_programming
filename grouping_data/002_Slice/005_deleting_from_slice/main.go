7c17fa4bf4e92669f7df76408cc622c920204a7a
Jesse Pinkman <babexexploitking@gmail.com>
2021-09-23 00:56:15 +0530

    Keep_going

---
+++ grouping_data/002_Slice/005_deleting_from_slice/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{11,22,33,44,55,66,4,6,7,8}
+	fmt.Println(x)
+
+	// we dont need a new function for deleting a slice we can do with append.
+
+	x = append(x[:4] , x[6:]...)
+	fmt.Println(x)
+}

65705c0271fb2922b04265357f0f4311eda9fd49
rootkit-io <babexexploitking@gmail.com>
2021-10-01 17:16:42 +0530

    keep_going

---
+++ grouping_data/003_Maps/002_maps_int/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := make(map[int]int)
+	x[1] = 10
+	fmt.Println(x[1])
+}
---
+++ grouping_data/003_Maps/003_maps_delete/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := make(map[string]int)
+	x["key"] = 10
+	fmt.Println(x["key"])
+	delete(x,"key")
+	fmt.Println(x)
+}
